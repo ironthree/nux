4ab51af7a404da55ce2addd96112bcf860c9722d
Fabio Valentini <decathorpe@gmail.com>
2021-09-15 21:58:39 +0200

    add docs for unit tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,20 @@
+//! minimal smoke tests for simple syscalls
+//!
+//! The unit tests in this module are expected to be simple, mostly for syscalls
+//! that can be called without arguments (or with very simple-to-construct
+//! arguments), with return values that are easy to check for errors, and
+//! that don't have any unwanted side effects.
+
 use super::syscalls;
 
 #[test]
-fn sys_39_getpid() {
+fn sched_yield() {
+    let ret = unsafe { syscalls::sched_yield() };
+    assert_eq!(ret, 0);
+}
+
+#[test]
+fn getpid() {
     let pid = unsafe { syscalls::getpid() };
     assert!(pid as i32 > 0);
 }
